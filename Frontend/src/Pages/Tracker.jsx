b5aade62b6bcb2df4ba8129add9762d37426248c
Jayesh17-11 <jayeshkanojia39@gmail.com>
2026-03-15 21:59:50 +0530

    fix leaflet marker icon

--- Frontend/src/Pages/Tracker.jsx
+++ Frontend/src/Pages/Tracker.jsx
@@ -7,7 +7,19 @@ import {
 } from "react-leaflet";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 const MIN_DISTANCE = 0.005;
 
 function Recenter({ position }) {
